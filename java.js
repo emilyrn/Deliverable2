c6483e4e3f119e15e3212ab3819a10d10bbeb266
emilyrn <emilyrn@bgsu.edu>
2017-04-05 00:51:03 -0400

    trying again

---
+++ java.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+
+function outputname() {
+  var y,x,name,a,b,answer;
+x=document.getElementById("form1");
+y=x.elements["name"].value;
+  document.getElementById("thisisconfusing").innerHTML="Thanks "+y+"<br>";
+  document.getElementById("thisisconfusing").innerHTML+="P.S. "+ y + " is a nice name :)";
+}
+
+</script>
